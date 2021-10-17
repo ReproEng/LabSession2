dcb12f0a1be44a6b883fecbcae04ba75ad130d00
Stefanie Scherzinger <stefanie.scherzinger@uni-passau.de>
2021-10-18 00:39:15 +0200

    Don't just consider the english-speaking world
    There are countries beyond the United Kingdom. Improve
    our greeting the world by using the user's native language
    for a greeting. Start with German, and use English in all
    other cases.
    
    Signed-off-by: Stefanie Scherzinger <stefanie.scherzinger@uni-passau.de>
    Reviewed-by: Wolfgang Mauerer <wolfgang.mauerer@othr.de>

--- hello.c
+++ hello.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char *hello(void) {
+  if (!strncmp(getenv("LANG"), "de_DE", 5)) {
+    return "Hallo, Welt!";
+  }
+
+  return "Hello, world";
+}
 
 void main() {
-  printf("Hello, world\n");
+  printf("%s\n", hello());
 }
